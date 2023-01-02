cada894131323c08eb1f1a3e185982f3f117afad
Zienab297 <60106900+Zienab297@users.noreply.github.com>
2023-01-02 20:09:36 +0200

    Add files via upload

---
+++ Trains.cpp
@@ -0,0 +1,6 @@
+﻿Build started...
+1>------ Build started: Project: sheet8, Configuration: Debug x64 ------
+1>sheet8.cpp
+1>sheet8.vcxproj -> E:\E-JUST\YEAR 2\Fall\Agorithms analysis\Sheets\sheet8\x64\Debug\sheet8.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Elapsed 00:01.742 ==========
